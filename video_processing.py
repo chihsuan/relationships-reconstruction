dabd7b9296cc6b999b87a0132285faec343a8b37
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-06 17:58:18 +0800

    capture face in video

--- video_processing.py
+++ video_processing.py
@@ -3,7 +3,7 @@
 
 '''
 This program is to detect the face in movie with two_entity_file and keyword_search_result (detect in specific frame)
-input: 1.movie_file (video format) 2.two_entity_file 3. search_result_file
+input: 1.movie_file (video format) 2. search_result_file
 
 '''
 
@@ -19,6 +19,7 @@ from modules import time_format
 from modules import cv_image
 
 FRAME_INTERVAL = 12
+EXPAND_TIME = 5
 OUTPUT_PATH = 'output/'
 
 class Pthread (threading.Thread):
@@ -36,7 +37,7 @@ class Pthread (threading.Thread):
         threadLock.release()
 
 
-def video_processing(movie_file, search_result_file:
+def video_processing(movie_file, search_result_file):
 
     # load frame-keyword files
     keyword_search_result = csv_io.read_csv(search_result_file)
@@ -44,56 +45,52 @@ def video_processing(movie_file, search_result_file:
     # load video
     videoInput = cv2.VideoCapture(movie_file)
 
-    # crate a start_frame to end_frame dictionary for two_entity_set look up
-    start_end = {}
-    for row in keyword_search_result:
-        start_frame, end_frame = row[]
-        while start_frame in start_end:
-            start_frame = start_frame + 0.001
-        while end_frame in start_end:
-            end_frame = end_frame + 0.001
-        start_end[start_frame] = end_frame 
-
     frame = {}
     face_count = 0
-    for keyword in two_entity_set:
-        for start_frame in two_entity_set[keyword]:
-            frame_position = int(start_frame) - 24 * 3
-            finish_frame = start_end[start_frame] + 24 * 3
-            while frame_position <= finish_frame: 
-                print keyword
-                videoInput.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_position)
-                flag, img = videoInput.read()
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                gray = cv2.equalizeHist(gray)
-                face_position_list, rects = cv_image.face_detect(gray, frame_position, (85, 85))
-                #face_position_list, rects =  faceDetection(gray, frame_position)
-                if 0xFF & cv2.waitKey(5) == 27:
-                    cv2.destroyAllWindows()
-                    sys.exit(1)
-                
-                if len(face_position_list) >= 1:
-                    print 'detected'
-                    # Role identify
-                    role_identify(rects, img)
-                    image_name = keyword + str(frame_position) + '.jpg'
-                    cv_image.output_image(rects, img, OUTPUT_PATH + '/img/' + image_name)
-                    for face_position in face_position_list:
-                        face_count += 1
-                        print face_count
-                        frame[face_count] = { 'keyword' : keyword, 
-                                                  'face_position': face_position.tolist(),
-                                                  'ID' : face_count,
-                                                  'frame_position': frame_position,
-                                                  'face_id': face_count} 
-                frame_position += FRAME_INTERVAL
+    for row in keyword_search_result:
+        start_frame, end_frame, keyword = float(row[0]), float(row[1]), row[2]
+        frame_position = round(start_frame) - 24 * EXPAND_TIME
+        finish_frame = round(end_frame) + 24 * EXPAND_TIME
+        while frame_position <= finish_frame: 
+            print keyword
+            videoInput.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_position)
+            flag, img = videoInput.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+            face_position_list, rects = cv_image.face_detect(gray, frame_position, (40, 40))
+            print face_position_list, rects
+            # Press ESC for close window 
+            if 0xFF & cv2.waitKey(5) == 27:
+                cv2.destroyAllWindows()
+                sys.exit(1)
+            
+            if len(face_position_list) >= 1:
+                print "detect face..."
+            
+                role_identify(rects, img)
+                image_name = keyword + str(frame_position) + '.jpg'
+                cv_image.output_image(rects, img, OUTPUT_PATH + '/img/' + image_name)
+                for face_position in face_position_list:
+                    face_count += 1
+                    print face_count
+                    frame[face_count] = { 'keyword' : keyword, 
+                                              'face_position': face_position.tolist(),
+                                              'ID' : face_count,
+                                              'frame_position': frame_position,
+                                              'face_id': face_count} 
+            frame_position += FRAME_INTERVAL
     #close video  
     videoInput.release()
 
     json_io.write_json(OUTPUT_PATH + 'frame.json', frame) 
 
 def role_identify(rect, img):
-    pass
+    '''for x1, y1, x2, y2 in rects:
+        face = img[y1:y2, x1:x2]
+        resize_face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)'''
+        #call openbr
+    return 
+        
 
 if __name__ == '__main__':
     video_processing(sys.argv[1], sys.argv[2])
