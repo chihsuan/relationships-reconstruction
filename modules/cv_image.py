152c70e349722ed90be22cf5c6ed8cbdd472608a
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-06 17:34:36 +0800

    update modules

--- modules/cv_image.py
+++ modules/cv_image.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 import cv2
 import cv2.cv as cv
 
@@ -9,13 +10,9 @@ OUTPUT_PATH = 'output/'
 def face_detect(img, framePosition, size):
 
     cascade = cv2.CascadeClassifier(HAAR_CASCADE_PATH)
-    #rects = cascade.detectMultiScale(img, 1.1, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,140))  #40 150  #25, 160 #20 140
-
-    #frame = cv2.CascadeClassifier(HAAR_CASCADE_PATH)
-    #rects = frame.detectMultiScale(img, 1.1, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20, 20) )  #40 150  #25, 160 #20 140
     rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4, minSize=(85, 85), flags = cv.CV_HAAR_SCALE_IMAGE)
     
-    cv2.imshow("movie", img)
+    #cv2.imshow("movie", img)
     if len(rects) == 0:
         return [], []
     rects[:,2:] += rects[:,:2]
@@ -25,8 +22,6 @@ def face_detect(img, framePosition, size):
     
     return face_position_list, rects
 
-    #if not os.path.isfile( OUTPUT_PATH + 'img/'+ name + '-1.jpg'):
-        #output_image(detected, image, name)
 
 def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
@@ -40,10 +35,3 @@ def output_image(rects, img, file_name):
         cv2.imshow("detected", show)
         resize_face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)
         cv2.imwrite( file_name, resize_face)
-    #background = cv2.imread( OUTPUT_PATH + 'img/0.jpg' )
-    #show = img.copy()
-    #saveImage = background.copy()
-    #saveImage[y1:y2, x1:x2] = face
-    #cv2.bitwise_and(saveImage, saveImage, face)
-    #cv2.imshow("detected", show)
-    #cv2.waitKey()
