e41c405511496174617a6dec9522926e2464ff84
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-07 13:15:37 +0800

    update video processing.py

--- video_processing.py
+++ video_processing.py
@@ -17,8 +17,9 @@ from modules import json_io
 from modules import csv_io
 from modules import time_format
 from modules import cv_image
+from modules import cv_face
 
-FRAME_INTERVAL = 12
+FRAME_INTERVAL = 6
 EXPAND_TIME = 5
 OUTPUT_PATH = 'output/'
 
@@ -37,10 +38,11 @@ class Pthread (threading.Thread):
         threadLock.release()
 
 
-def video_processing(movie_file, search_result_file):
+def video_processing(movie_file, search_result_file, role_list_file):
 
     # load frame-keyword files
     keyword_search_result = csv_io.read_csv(search_result_file)
+    role_list = csv_io.read_csv(role_list_file)
 
     # load video
     videoInput = cv2.VideoCapture(movie_file)
@@ -49,15 +51,16 @@ def video_processing(movie_file, search_result_file):
     face_count = 0
     for row in keyword_search_result:
         start_frame, end_frame, keyword = float(row[0]), float(row[1]), row[2]
-        frame_position = round(start_frame) - 24 * EXPAND_TIME
-        finish_frame = round(end_frame) + 24 * EXPAND_TIME
+        print start_frame, end_frame
+        frame_position = round(start_frame) #- 24 * EXPAND_TIME
+        finish_frame = round(end_frame) #+ 24 * EXPAND_TIME
         while frame_position <= finish_frame: 
             print keyword
             videoInput.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_position)
             flag, img = videoInput.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             gray = cv2.equalizeHist(gray)
-            face_position_list, rects = cv_image.face_detect(gray, frame_position, (40, 40))
+            face_position_list, rects = cv_image.face_detect(gray, frame_position, (30, 30))
             print face_position_list, rects
             # Press ESC for close window 
             if 0xFF & cv2.waitKey(5) == 27:
@@ -67,12 +70,13 @@ def video_processing(movie_file, search_result_file):
             if len(face_position_list) >= 1:
                 print "detect face..."
             
-                role_identify(rects, img)
-                image_name = keyword + str(frame_position) + '.jpg'
-                cv_image.output_image(rects, img, OUTPUT_PATH + '/img/' + image_name)
+                image_name = OUTPUT_PATH + 'img/' + keyword + str(int(frame_position))  
+                cv_image.output_image(rects, img, image_name)
+                face_number = 0
                 for face_position in face_position_list:
+                    print role_identify( image_name + '-' + str(face_number) + '.jpg', role_list)
+                    face_number += 1
                     face_count += 1
-                    print face_count
                     frame[face_count] = { 'keyword' : keyword, 
                                               'face_position': face_position.tolist(),
                                               'ID' : face_count,
@@ -84,13 +88,14 @@ def video_processing(movie_file, search_result_file):
 
     json_io.write_json(OUTPUT_PATH + 'frame.json', frame) 
 
-def role_identify(rect, img):
-    '''for x1, y1, x2, y2 in rects:
-        face = img[y1:y2, x1:x2]
-        resize_face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)'''
-        #call openbr
-    return 
+def role_identify(img_name, role_list):
+    similarity_rate = {}
+    for role in role_list:
+        img2_name = 'input/roles/' + role + '.jpg' 
+        rate = cv_face.reg(img_name, img2_name)
+        similarity_rate[role] = rate
+    return max(similarity_rate, key=similarity_rate.get)
         
 
 if __name__ == '__main__':
-    video_processing(sys.argv[1], sys.argv[2])
+    video_processing(sys.argv[1], sys.argv[2], sys.argv[3])
