4f4832db7684b6069a093643f89de68c853b0e2c
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-07 17:37:37 +0800

    update bipartite_garph.py add structure

--- bipartite_graph.py
+++ bipartite_graph.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+import sys
+
+from modules import json_io
+from modules import csv_io
+
+def build_bipartite_graph(keyword_dic_file):    
+    
+    keyword_dic = json_io.read_json(keyword_dic_file)
+
+
+def convergence_graph:
+    pass
+def set_weight:
+    pass
+
+if __name__=='__main__':
+    keyword_search(sys.argv[1], sys.argv[2])
