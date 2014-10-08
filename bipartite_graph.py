34a428e5adf75a802a533e419179651f100fa758
chihsuan <chihsuan.huang.7@gmail.com>
2014-10-08 10:00:58 +0800

    update grpah function

--- bipartite_graph.py
+++ bipartite_graph.py
@@ -8,12 +8,13 @@ from modules import csv_io
 def build_bipartite_graph(keyword_dic_file):    
     
     keyword_dic = json_io.read_json(keyword_dic_file)
+    pair_bipartite_graph = to_pair(keyword_dic)
 
+def to_pair(keyword_dic):
 
-def convergence_graph:
-    pass
-def set_weight:
-    pass
+    pair_bipartite_graph = {}
+    for keyword_t in keyword_dic:
+        pair_bipartite_graph[keyword_t] =
 
 if __name__=='__main__':
-    keyword_search(sys.argv[1], sys.argv[2])
+    build_bipartite_graph(sys.argv[1])
