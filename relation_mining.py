9805967ea32fbd8c3da71efff7fa56a6d64de654
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-10 19:12:24 +0800

    update relation_mining.py

---
+++ relation_mining.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import sys
+
+from modules import json_io
+from modules import csv_io
+from modules import data_transform
+
+
+def relation_mining():
+    pass
+
+
+if __name__ == '__main__':
+    relation_mining(sys.argv[1])
+
