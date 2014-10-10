b2a9c1496862343ad64056f45f80c7f84916b176
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-10 19:13:25 +0800

    add relation mining data and data_transfrom.py

---
+++ modules/data_transform.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+import sys
+
+import json_io
+import csv_io
+
+def data_to_json(input_file):
+    
+    csv_data = csv_io.read_csv(input_file)
+
+    relation = {}
+    for row in csv_data:
+        relation[row[0]] = {}
+        for i in range(1, len(row)):
+            relation[row[0]][row[i]] = {'know': 1}
+
+    json_io.write_json('output/social_graph.json', relation)
+
+if __name__=='__main__':
+    data_to_json(sys.argv[1])
