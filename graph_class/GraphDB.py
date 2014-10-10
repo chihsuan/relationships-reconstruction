38b5bb4c36917a8dfef39b1d9b49eb0fedcfdb4d
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-10 19:18:31 +0800

    update GraphDB.py

--- graph_class/GraphDB.py
+++ graph_class/GraphDB.py
@@ -5,27 +5,59 @@
 
 import sys
 
-from neo4jrestclient.client import GraphDatabase
+from neo4j import GraphDatabase
 
 from modules import json_io
 from modules import csv_io
 
-def neo4j_db(neo4j_url, data_path):
+class GraphDB:
 
-    gdb = GraphDatabase(neo4j_url)
-    data = json_io.read_json(data_path)
-    roles = {}
+    def __init__(self, neo4j_url="graph.db"):
+        self.db = GraphDatabase(neo4j_url) 
+        self.nodes = {}
+        self.rels = []
 
-    for source_name, targets in data.iteritems():
-        source = gdb.nodes.create(name=source_name)
-        for target_name in targets:
-            if target_name in roles:
-                target = roles[target_name]
-            else:
-                target = gdb.nodes.create(name=target_name) 
-                roles[target_name] = target
-            for attr, val in targets[target_name].iteritems():
-                source.relationships.create(str(attr), target, value=val)
+    def file_to_db(self, data_path):
+        data = json_io.read_json(data_path)
+    
+        with self.db.transaction:
+            for source_name, targets in data.iteritems():
+                if source_name in self.nodes:
+                    source = self.nodes[source_name]
+                else:
+                    source = self.db.node(name=source_name)
+                    self.nodes[source_name] = source
+                for target_name in targets:
+                    if target_name in self.nodes:
+                        target = self.nodes[target_name]
+                    else:
+                        target = self.db.node(name=target_name) 
+                        self.nodes[target_name] = target
+                    #for attr, val in targets[target_name].iteritems():
+                    self.rels.append(source.knows(target))
+            return self.nodes
 
-if __name__=='__main__':
-    neo4j_db("http://localhost:7474/db/data/", sys.argv[1])
+            
+    def pattern_matching(self, source_name, target_name, keyword):
+        source = self.nodes[source_name]
+        target = self.nodes[target_name]
+        q = '''START me=node({s_id}) MATCH (me)-[:knows]->(remote_friend) \
+                WHERE me.name = {s_name} RETURN remote_friend'''
+        result = self.db.query(q, s_id=source.id ,s_name=source_name)
+        
+        for result in result:
+            for name in result['remote_friend'].values():
+                print name
+
+    def relationship_tagging(self, pair, target, keyword):
+        pass
+
+    def clear(self):
+        with self.db.transaction:
+            for rel in self.db.relationships:
+                rel.delete()
+            for node in self.db.nodes:
+                node.delete()
+
+    def shutdown(self):
+        self.db.shutdown()
