68a809f8ef429841fb65a2bf298837c35017c526
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-11 15:26:33 +0800

    relations to json

--- relation_mining.py
+++ relation_mining.py
@@ -8,10 +8,9 @@ from modules import csv_io
 from modules import data_transform
 
 
-def relation_mining():
-    pass
-
+def relation_mining(input_way, relation_file):
+    data_transform.to_json(relation_file)
 
 if __name__ == '__main__':
-    relation_mining(sys.argv[1])
+    relation_mining(sys.argv[1], sys.argv[2])
 
