ddf6ed32734021851f5ee2370dc4a3e1652b9563
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-11 15:18:48 +0800

    refactoring video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -25,7 +25,7 @@ EXPAND_TIME = 5
 OUTPUT_PATH = 'output/'
 roles_foldr = 'input/roles/'
 
-def video_processing(movie_file, search_result_file, role_list_file):
+def video_processing(movie_file, search_result_file, role_list_file, role_input_way):
 
     # load frame-keyword files
     keyword_search_result = csv_io.read_csv(search_result_file)
@@ -36,7 +36,7 @@ def video_processing(movie_file, search_result_file, role_list_file):
 
     frame = {}
     keyword_id = 0
-    face_number = 0
+    frame_number = 0
     for row in keyword_search_result:
 
         start_frame, end_frame, keyword = float(row[0]), float(row[1]), row[2]
@@ -46,54 +46,65 @@ def video_processing(movie_file, search_result_file, role_list_file):
         keyword_id += 1
         keyword_time = keyword + '_t' + str(keyword_id)
         while frame_position <= finish_frame: 
-            videoInput.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_position)
-            flag, img = videoInput.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            gray = cv2.equalizeHist(gray)
-            face_position_list, rects = cv_image.face_detect(gray, frame_position, (40, 40))
-            
-            # Press ESC for close window 
-            if 0xFF & cv2.waitKey(5) == 27:
-                cv2.destroyAllWindows()
-                sys.exit(1)
+
+            face_position_list, rects, img = frame_caputre(videoInput, frame_position)
             
             if len(face_position_list) >= 1:
                 print "detect face..."
                 
-                image_name = OUTPUT_PATH + 'img/' + keyword_time + str(face_number) 
+                image_name = OUTPUT_PATH + 'img/' + keyword_time + str(frame_number) 
                 cv_image.output_image(rects, img, image_name)
               
                 count = 0
                 for face_position in face_position_list:
-                    '''role_name = role_identify( image_name + '-' + \
-                                            str(count) + '.jpg', role_list)'''
-                    print len(face_position_list)
-                    print role_list
-                    role_name = raw_input('name? ')
-                    try:
-                        role_name = role_list[int(role_name)-1]
-                    except:
-                        role_name = raw_input('name? ')
+                    
+                    if role_input_way == 0:
+                        role_name = role_identify( image_name + '-' + str(count) + '.jpg', role_list)
+                    else:
+                        role_name = role_input(role_list)
                     count += 1
-                    face_number += 1
                     if role_name == []:
                         continue
-                    if keyword_time not in frame:
-                        frame[keyword_time] = {}  
-                    if role_name in frame[keyword_time]:
-                        frame[keyword_time][role_name]['weight'] += 1
                     else:
-                        frame[keyword_time][role_name] = {'keyword' : keyword, 
-                                                          'face_position' : face_position.tolist(),
-                                                          'frame_position' : frame_position,
-                                                          'keyword_id' : keyword_id,
-                                                          'weight' : 1 } 
+                        if keyword_time not in frame:
+                            frame[keyword_time] = {}  
+                        if role_name in frame[keyword_time]:
+                            frame[keyword_time][role_name]['weight'] += 1
+                        else:
+                            frame[keyword_time][role_name] = {'keyword' : keyword, 
+                                                              'face_position' : face_position.tolist(),
+                                                              'frame_position' : frame_position,
+                                                              'keyword_id' : keyword_id,
+                                                              'weight' : 1 } 
+            frame_number += 1
             frame_position += FRAME_INTERVAL
 
     #close video  
     videoInput.release()
 
     json_io.write_json(OUTPUT_PATH + 'keywordt_roles.json', frame) 
+
+def frame_caputre(videoInput, frame_position):
+    videoInput.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_position)
+    flag, img = videoInput.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    face_position_list, rects = cv_image.face_detect(gray, frame_position, (40, 40))
+    
+    if 0xFF & cv2.waitKey(5) == 27:
+                cv2.destroyAllWindows()
+                sys.exit(1)
+       
+    return face_position_list, rects, img
+
+def role_input(role_list):
+    print role_list
+    role_name = raw_input('name? ')
+    try:
+        role_name = role_list[int(role_name)-1]
+    except:
+        role_name = raw_input('name? ')
+    return role_name
 
 def role_identify(img_name, role_list):
     pattern = re.compile(r'(\D+)\d?\.') 
@@ -120,4 +131,4 @@ def role_identify(img_name, role_list):
         
 
 if __name__ == '__main__':
-    video_processing(sys.argv[1], sys.argv[2], sys.argv[3])
+    video_processing(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
